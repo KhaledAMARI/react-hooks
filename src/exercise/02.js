da4f69d8dd5ac568e78bf3fb15db372c2e950084
khaledamari <khaled.amari@1spatial.com>
2022-09-05 17:11:03 +0100

    - Learning useEffect hook. - fetching data from localStorage and update our state with it. - use lazy initialization to avoid a performance bottleneck of reading into localStorage on every render. - Add dependencies array to useEffect to avoid the callback beign called too frequently. - Create a custom hook for reusability of a logic. - make the custom hook flexible with all type of entred data.

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,14 +3,38 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
+function useLocalStorageState(key, defaultValue, {serialize = JSON.stringify, deserialize = JSON.parse  } = {}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
-
+  const [name, setName] = React.useState(
+    () => {
+      let valueFromLocalStrorage = window.localStorage.getItem(key);
+      if (valueFromLocalStrorage) {
+        return deserialize(valueFromLocalStrorage)
+      }
+      return defaultValue
+    }
+  )
+    const prevKeyRef = React.useRef(key);
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
+  React.useEffect(() => {
+    const prevKey = prevKeyRef.current;
+    if (prevKey !== key) {
+      localStorage.removeItem(prevKey);
+    };
+    prevKeyRef.current = key;
+    window.localStorage.setItem(key, serialize(name))
+  }, [key, name, serialize])
+
+  console.log(name);
+  return [name, setName];
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName);
+  // const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     setName(event.target.value)
@@ -30,4 +54,4 @@ function App() {
   return <Greeting />
 }
 
-export default App
+export default App;
